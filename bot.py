884d93f1d39c3facb527b35ada2fd20b52f20815
mopifish <87352493+mopifish@users.noreply.github.com>
2023-06-12 19:00:07 -0700

    V 1.0.0 -Rebalanced Hunger and Sleep Numbers -Gatito Takes damage from Empty stats now -Gatito heals if stats are above 90% -Reworked command system into RESPONSES dict for easier command addition -Added dance command -Added pet command -Added sniff command -Gatito's Status is now routinely saved to a file -Gatito now ages in days, age can be viewed in status -Gatito can now die -Gatito can now be resurrected -Gatito has a number indicating how many times he's been resurrected, displayed next to his name -Restored food lists, gatito can now properly react to a variety of foods -Gatito now keeps track of loved foods, liked foods, hated foods, and allergic foods via several commands -Gatitos mood now updates in response to messages

--- bot.py
+++ bot.py
@@ -3,11 +3,8 @@
 import discord
 from discord.ext import tasks
 from datetime import datetime, time
-import asyncio
-import icecream as debug
 
 import config
-
 import gatito as gt
 
 
@@ -19,14 +16,18 @@ def run_discord_bot():
     intents.members = True
     client = discord.Client(intents=intents)
 
-    gatito = gt.Gatito(1)
+    gatito = gt.Gatito("stats.json")
 
     @client.event
     async def on_ready():
         update_status.start()
         update_stats.start()
-        #send_status_update.start()
+        update_age.start()
+
+        send_status_update.start()
         print(f"{client.user} is now running!")
+
+        await client.change_presence(status=discord.Status.idle, activity=discord.Game(name=gatito.get_mood()))
 
     @client.event
     async def on_message(message):
@@ -35,34 +36,41 @@ def run_discord_bot():
 
         print(f"{message.author} said: {message.content}")
 
+        old_gatito_mood = gatito.get_mood()
         await gatito.process(message)
 
+        if gatito.get_mood() != old_gatito_mood:
+            status = discord.Status.online
+            if gatito.state == gt.States.SLEEPING:
+                status = discord.Status.idle
+            if gatito.state == gt.States.DEAD:
+                status = discord.Status.dnd
+            await client.change_presence(status=status, activity=discord.Game(name=gatito.get_mood()))
+
+
+    # FIXME: Maybe scrap this method?
     @tasks.loop(minutes=1)
     async def update_status():
-        gatito.update_mood()
-        status = discord.Status.online
-
-        if gatito.status == gt.Status.SLEEPING:
-            status = discord.Status.idle
-        elif gatito.status == gt.Status.BAD:
-            status = discord.Status.dnd
-
-        await client.change_presence(status=status, activity=discord.Game(name=gatito.mood))
+        if client.user.name != "Gatito " + str(gatito.stats["number"]) if gatito.stats["number"] > 1 else "":
+            await client.user.edit(username="Gatito " + str(gatito.stats["number"]) if gatito.stats["number"] > 1 else "")
 
     @tasks.loop(minutes=15)
     async def update_stats():
-        gatito.add_hunger(-1)
-        gatito.add_sleep(1 if gatito.sleeping else -1)
+        gatito.update_stats()
+        gatito.save()
 
-    # @tasks.loop(minutes=1)
-    # async def send_status_update():
-    #     now = datetime.now().time()
-    #     AM = datetime.now().strftime("%p") == "AM"
-    #     PM = datetime.now().strftime("%p") == "PM"
-    #
-    #     #TODO: Properly set up status printing, once 24 hour hosting
-    #
-    #     # if (now == time(8, 00) and AM) or (now == time(9, 45) and PM):
-    #     #     await gt.messaging.send_message(gatito.respond("!status")[0], [client.get_channel(int(1112954436076179576))])
+    @tasks.loop(hours=24)
+    async def update_age():
+        gatito.add_age(1)
+
+    @tasks.loop(minutes=1)
+    async def send_status_update():
+        now = datetime.now().time()
+        AM = datetime.now().strftime("%p") == "AM"
+        PM = datetime.now().strftime("%p") == "PM"
+
+        if (now == time(8, 00) and AM) or (now == time(9, 45) and PM):
+            await gt.messaging.send_message(gatito.respond("!status")[0],
+                                            [client.get_channel(int(1112954436076179576))])
 
     client.run(TOKEN)
--- gatito.py
+++ gatito.py
@@ -1,39 +1,50 @@
 """ Module for Gatito Class"""
 
-# TODO: Gatito takes damage from empty stats
-# TODO: Let gatito die
-# TODO: Rebalance stat loss
-# --- JSON Work ---
-# TODO: Save stats
-# TODO: Redo the food list
+# PLAY UPDATE: V 1.1.0
+# TO/DO: Add more moods
+# TO/DO: Add play/happiness meter
+# TO/DO: Add games
+
+# RELEASE: V 1.0.0
+# TODO: Consider mapping function calls to possible dict responses?
+# TODO: Tidy up bot.py, and consider chaning the bot.py > gatito hierarchy
 # --- COMMANDS ---
-# TODO: Pet Command
-# TODO: Wakeup command (send someone messages till they respond)
-# TODO: Announce command
+# - Wakeup command (send someone messages till they respond)
+# - Announce command
+# - Drink command?
+# - Dressup command?
 
 import discord
-import emoji
 from enum import Enum
 import json
-import math
+import random
 
 import messaging
 import icecream as debug
 
+# The following have potential for their own class or file
 COMMAND_SYMBOL = "!"
 COMMANDS = {
-    "wakeup": ["!goodmorning", "!wakeup", "!morning"],
+    "wakeup": ["!goodmorning", "!wakeup", "!morning", "!wake"],
     "spit": ["!spit", "!ptoeey"],
     "punch": ["!punch"],
     "slap": ["!slap"],
     "kiss": ["!kiss"],
+    "dance": ["!dance", "!play", "!jump", "!yippee", "!happy"],
+    "pet": ["!pet", "!scritch"],
+    "sniff": ["!sniff", "!jeffthekiller"],
+
     "sleep": ["!goodnight", "!sleep", "!night"],
+    "bestfoods": ["!bestfoods", "!favorites", "!lovedfoods", "!yummys", "!yummies"],
+    "goodfoods": ["!goodfoods", "!likes", "!goods", "!likedfoods"],
+    "badfoods": ["!badfoods", "!bads", "!hates", "!hatedfoods"],
+    "poisonfoods": ["!poisonousfoods", "!poison", "!poisons", "!dangerfoods", "!poisonfoods"],
     "eat": ["!eat"],
-    "status": ["!status"]
+    "drink": ["!drink", "!slurp"],
+    "status": ["!status"],
+    "resurrect": ["!resurrect", "!live", "!rise", "!revive"]
 
 }
-
-
 def get_all_commands():
     commands_list = []
     for k, v in COMMANDS.items():
@@ -42,36 +53,46 @@ def get_all_commands():
 
     return commands_list
 
+RESPONSES = {
+    "wakeup": ["ai'm!! Awaek!!!", "ghuhgh!! Hugh!GH!!", "stopp EIT!!!!"],
+    "spit": ["💦💦💦"],
+    "punch": ["👊💥💥"],
+    "slap": ["🫲💥💥"],
+    "kiss": ["media/cat-kiss-kiss-cat.gif"],
+    "dance": ["media/dance.gif"],
+    "pet": ["media/normal_pet.gif", "media/fast_pet.gif", "media/slow_pet.gif", "media/squishy_pet.gif"],
+    "sniff": ["media/sniff.gif"],
 
-class Status(Enum):
-    GOOD = 1
-    OKAY = 2
-    BAD = 3
-    SLEEPING = 4
+    "dead": ["media/dead.gif", "media/dead2.gif", "media/dead3.gif", "media/dead4.gif"],
+}
 
+class States(Enum):
+    AWAKE = 1,
+    SLEEPING = 2,
+    DEAD = 3
 
 class Gatito:
-    def __init__(self, number):
-        with open("stats.json") as f:
-            self.stats = json.load(f)
+    def __init__(self, stats_path, number=None):
+        try:
+            # Attempts to load stats.json
+            with open(stats_path) as f:
+                self.stats = json.load(f)
+        except FileNotFoundError:
+            # If no stats are found (indicating fresh copy) new stats are created
+            with open("stats.default.json") as f:
+                self.stats = json.load(f)
+            self.save()
 
-        # Gatito Initialization
-        self._number = number
-        self._age = 1
-
-        self.mood = "Okay"
-        self.status = Status.GOOD
-
-        self.max_health = 100
-        self.health = self.max_health
-        self.max_hunger = 100
-        self.hunger = self.max_hunger
-        self.max_sleep = 100
-        self.sleep = self.max_sleep
-
-        self.sleeping = True
+        if number is not None: self.stats["number"] = number
+        self.state = States.SLEEPING
 
         self.previous_target = None
+
+
+
+    def save(self):
+        with open("stats.json", "w") as f:
+            json.dump(self.stats, f)
 
     async def process(self, message):
         targets = messaging.get_all_targets(message)
@@ -81,7 +102,10 @@ class Gatito:
 
         self.previous_target = targets
         for pair in actions:
-            pair[0](pair[1])
+            try:
+                pair[0](pair[1])
+            except TypeError:
+                pair()
 
     def respond(self, message):
         if COMMAND_SYMBOL not in message:
@@ -97,97 +121,157 @@ class Gatito:
             embed = self.get_status_embed()
             responses.append(embed)
 
-        # --- Sleeping Commands ---
-        elif self.sleeping:
+        if self.state == States.DEAD:
+            # Responds with a random "dead" gif if any command is called
             if command[0] in COMMANDS["wakeup"]:
-                responses.append("media/wakeup.gif")
-                actions.append((self.set_sleeping, False))
+                responses.append(random.choice(RESPONSES["dead"]))
+                actions.append((self.set_state, States.SLEEPING))
+            elif command[0] in COMMANDS["resurrect"]:
+                responses.append("🪄✨💖✨💖")
+                responses.append("💢GROOUUAHHHHHHH💢")
+                actions.append(self.resurrect)
             elif command[0] in get_all_commands():
                 responses = ["mimimimmemimim 💤💤(shh! he's sleeping!)"]
 
-        # --- Awake Commands ---
-        else:
+        elif self.state == States.SLEEPING:
             if command[0] in COMMANDS["wakeup"]:
-                responses.append("ai'm!! Awaek!!!")
+                responses.append("media/wakeup.gif")
+                actions.append((self.set_state, States.AWAKE))
+            elif command[0] in get_all_commands():
+                responses = ["mimimimmemimim 💤💤(shh! he's sleeping!)"]
 
-            elif command[0] in COMMANDS["spit"]:
-                responses.append("💦💦💦")
+        elif self.state == States.AWAKE:
+            # Handles all genertic text responses
+            for k, v in COMMANDS.items():
+                if command[0] in v and k in RESPONSES:
+                    responses.append(random.choice(RESPONSES[k]))
 
-            elif command[0] in COMMANDS["punch"]:
-                responses.append("👊💥💥")
-
-            elif command[0] in COMMANDS["slap"]:
-                responses.append("🫲💥💥")
-
-            elif command[0] in COMMANDS["kiss"]:
-                responses.append("media/cat-kiss-kiss-cat.gif")
-
-            elif command[0] in COMMANDS["sleep"]:
+            # More complex and action commands
+            if command[0] in COMMANDS["sleep"]:
                 responses.append("media/yawn.gif")
-                actions.append((self.set_sleeping, True))
+                actions.append((self.set_state, States.SLEEPING))
+
+            elif command[0] in COMMANDS["bestfoods"]:
+                responses.append(("I love love love theese! " + "".join(self.stats["known_best_foods"])) if len(
+                    self.stats["known_best_foods"]) else "I don lolove anyfing........")
+            elif command[0] in COMMANDS["goodfoods"]:
+                responses.append(("I like theese! " + "".join(self.stats["known_good_foods"])) if len(
+                    self.stats["known_good_foods"]) else "I don likke anyfing........")
+            elif command[0] in COMMANDS["badfoods"]:
+                responses.append(("Thees arr GROSS!!! " + "".join(self.stats["known_bad_foods"])) if len(
+                    self.stats["known_bad_foods"]) else "I like everyfing!! Nuffins yuckyee")
+            elif command[0] in COMMANDS["poisonfoods"]:
+                responses.append(("Thees HURTTT!!!!!! " + "".join(self.stats["known_poisonous_foods"])) if len(
+                    self.stats["known_poisonous_foods"]) else "Ai'm Invincble, nuffin hurts me!!")
 
             elif command[0] in COMMANDS["eat"] and len(command) > 1:
-
                 for food in command[1:]:
                     if food in self.stats["BEST_FOODS"]:
                         responses.append("Sloorp mm Glorrp SLurp!! I love " + food)
-                        actions.append((self.add_hunger, 5))
+                        actions.append((self.add_hunger, 15))
+
+                        # Discovers food if it is undiscovered
+                        if food not in self.stats["known_best_foods"]: self.stats["known_best_foods"].append(food)
                     elif food in self.stats["GOOD_FOODS"]:
                         responses.append("Armf Narmf Narmf! Mmm " + food)
-                        actions.append((self.add_hunger, 3))
+                        actions.append((self.add_hunger, 10))
+
+                        # Discovers food if it is undiscovered
+                        if food not in self.stats["known_good_foods"]: self.stats["known_good_foods"].append(food)
                     elif food in self.stats["BAD_FOODS"]:
-                        actions.append((self.add_hunger, 1))
+                        actions.append((self.add_hunger, 10))
                         responses.append("Steenky!! I hate " + food)
+
+                        # Discovers food if it is undiscovered
+                        if food not in self.stats["known_bad_foods"]: self.stats["known_bad_foods"].append(food)
                     elif food in self.stats["POISONOUS_FOODS"]:
                         responses.append("HYYYEUUUUUCKKKKK PPEEYEYPPEYEEWWW! 👎👎 " + food + "❗❗")
-                        actions.append((self.add_health, -1))
+                        actions.append((self.add_health, -5))
+
+                        # Discovers food if it is undiscovered
+                        if food not in self.stats["known_poisonous_foods"]: self.stats["known_poisonous_foods"].append(
+                            food)
                     else:
                         responses.append("OUCHYYY I can NAWT eet THEIS!❗❗")
                         actions.append((self.add_health, -1))
+                    # elif command[0] in COMMANDS["drink"] and len(command) > 1:
+                    #     responses.append("Glug glug gloogl glugl")
+                    #     for drink in command[1:]:
+                    #         if drink == emoji.emojize(":milk:"):
+                    #             responses.append("PFP:media/gatito.png")
+                    #         elif drink == emoji.emojize(":beer:"):
+                    #             responses.append("PFP:media/clonetito.png")
 
         return responses, actions
 
     # ------------------------------------ UPDATE METHODS ------------------------------------
-    def update_mood(self):
-        self.mood = "Good"
-        self.status = Status.GOOD
+    def get_mood(self):
+        mood = "Good"
 
-        if self.hunger < 50:
-            self.mood = "Hungry"
-            self.status = Status.OKAY
+        if self.state == States.AWAKE:
+            if self.stats["hunger"] < 50:
+                mood = "Hungry"
+            if self.stats["hunger"] < 20:
+                mood = "Starving"
 
-        if self.hunger < 20:
-            self.mood = "Starving"
-            self.status = Status.BAD
+        elif self.state == States.SLEEPING:
+            mood = "Eeping 💤💤"
+        elif self.state == States.DEAD:
+            mood = "☠️🦴DEAD💀🦴"
 
-        if self.sleeping:
-            self.mood = "Eeping 💤💤"
-            self.status = Status.SLEEPING
+        return mood
+
+    def update_stats(self):
+        self.add_hunger(-2)
+        self.add_sleep(2 if self.state == States.SLEEPING else -2)
+
+        if self.stats["hunger"] <= 0:
+            self.add_health(-5)
+        elif self.stats["hunger"] >= 0.9 * self.stats["MAX_HUNGER"]:
+            self.add_health(5)
+        if self.stats["sleep"] <= 0:
+            self.add_health(-5)
+        if self.stats["sleep"] >= 0.9 * self.stats["MAX_SLEEP"]:
+            self.add_health(5)
+
+    def resurrect(self):
+        self.__init__("stats.default.json", self.stats["number"] + 1)
 
     # ------------------------------------ GET METHODS ------------------------------------
     def get_status_embed(self):
-        embed = discord.Embed(title="🌟GATITO STATUS🌟", description="Feeling: " + "mood", color=0x00ff00)
-        embed.add_field(name="HEALTH" + str(self.health) + "/" + str(self.max_health),
-                        value=emoji.emojize(":heart:") * math.ceil(self.health / 10) + emoji.emojize(":broken_heart:") * math.ceil((self.max_health - self.health) / 10), inline=False)
-        embed.add_field(name="HUNGER: " + str(self.hunger) + "/" + str(self.max_hunger),
-                        value="🟩" * math.ceil(self.hunger / 10) + "🟥" * math.ceil((self.max_hunger - self.hunger) / 10), inline=False)
-        embed.add_field(name="SLEEP: " + str(self.sleep) + "/" + str(self.max_sleep),
-                        value="🟩" * math.ceil(self.sleep / 10) + "🟥" * math.ceil((self.max_sleep - self.sleep) / 10), inline=False)
+        embed = discord.Embed(title="🌟GATITO STATUS🌟",
+                              description="Feeling: " + self.get_mood() + "\nAge: " + str(self.stats["age"]) + " days",
+                              color=0x00ff00)
+        embed.add_field(name="HEALTH: " + str(self.stats["health"]) + "/" + str(self.stats["MAX_HEALTH"]),
+                        value="❤️" * round(self.stats["health"] / 10) + "💔" * round(
+                            (self.stats["MAX_HEALTH"] - self.stats["health"]) / 10), inline=False)
+        embed.add_field(name="HUNGER: " + str(self.stats["hunger"]) + "/" + str(self.stats["MAX_HUNGER"]),
+                        value="🟩" * round(self.stats["hunger"] / 10) + "🟥" * round(
+                            (self.stats["MAX_HUNGER"] - self.stats["hunger"]) / 10),
+                        inline=False)
+        embed.add_field(name="SLEEP: " + str(self.stats["sleep"]) + "/" + str(self.stats["MAX_SLEEP"]),
+                        value="🟩" * round(self.stats["sleep"] / 10) + "🟥" * round(
+                            (self.stats["MAX_SLEEP"] - self.stats["sleep"]) / 10),
+                        inline=False)
         return embed
 
     # ------------------------------------ SETTERS ------------------------------------
     def add_health(self, value):
-        self.health += value
-        self.health = max(min(self.health, self.max_health), 0)
+        self.stats["health"] += value
+        self.stats["health"] = max(min(self.stats["health"], self.stats["MAX_HEALTH"]), 0)
+
+        if self.stats["health"] == 0: self.state = States.DEAD
 
     def add_hunger(self, value):
-        self.hunger += value
-        self.hunger = max(min(self.hunger, self.max_hunger), 0)
+        self.stats["hunger"] += value
+        self.stats["hunger"] = max(min(self.stats["hunger"], self.stats["MAX_HUNGER"]), 0)
 
     def add_sleep(self, value):
-        self.sleep += value
-        self.sleep = max(min(self.sleep, self.max_sleep), 0)
+        self.stats["sleep"] += value
+        self.stats["sleep"] = max(min(self.stats["sleep"], self.stats["MAX_SLEEP"]), 0)
 
-    def set_sleeping(self, value):
-        self.sleeping = value
+    def add_age(self, value):
+        self.stats["age"] += value
 
+    def set_state(self, state):
+        self.state = state
